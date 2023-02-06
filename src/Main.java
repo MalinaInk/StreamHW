238e4bd9e8e81057e1f9142f15fa4097c081f1c6
mo <nataleshen.ko@yandex.ru>
2023-02-06 15:52:40 +0300

    commit-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("r");
+        findMinMax(Stream.of("test", "t", "tes", "testtest"),
+                Comparator.comparingInt(String::length),
+                (min, max) -> System.out.println("min = " + min + ", max = " + max));
+
+        task1Stream();
+
+    }
+    private static void task1Stream() {
+        Random r = new Random();
+        IntStream stream = r.ints(10L, 0,100);
+        stream.filter(i -> i % 2 == 0)
+                .forEach(System.out::println);
+    }
+
+
+    private static <T> void findMinMax(Stream<? extends T> stream,
+                                       Comparator<? super T> comparator,
+                                       BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.sorted(comparator).collect(Collectors.toList());
+        minMaxConsumer.accept(list.get(0), list.get(list.size()-1));
     }
 }
